788f1abed32b2b1ae488d25fc6dd6a6749c29046
Alex Nitu <novalex.inc@gmail.com>
2018-10-26 00:45:33 +0300

    Update build files.

--- build/index.js
+++ build/index.js
@@ -7,6 +7,7 @@
 var _require = require('electron'),
     app = _require.app,
     dialog = _require.dialog,
+    Menu = _require.Menu,
     BrowserWindow = _require.BrowserWindow;
 
 var windowStateKeeper = require('electron-window-state');
@@ -61,11 +62,47 @@ function createWindow() {
 	});
 }
 
+function createMenu() {
+	// Menu template.
+	var menuTemplate = [{
+		label: 'View',
+		submenu: [{ role: 'reload' }, { role: 'forcereload' }, { role: 'toggledevtools' }, { type: 'separator' }, { role: 'resetzoom' }, { role: 'zoomin' }, { role: 'zoomout' }, { type: 'separator' }, { role: 'togglefullscreen' }]
+	}, {
+		role: 'window',
+		submenu: [{ role: 'minimize' }, { role: 'close' }]
+		// {
+		// 	role: 'help',
+		// 	submenu: [
+		// 		{
+		// 			label: 'Learn More',
+		// 			click () { require('electron').shell.openExternal('https://electronjs.org') }
+		// 		}
+		// 	]
+		// }
+	}];
+
+	if (process.platform === 'darwin') {
+		menuTemplate.unshift({
+			label: app.getName(),
+			submenu: [{ role: 'about' }, { type: 'separator' },
+			// { role: 'services', submenu: [] },
+			{ type: 'separator' }, { role: 'hide' }, { role: 'hideothers' }, { role: 'unhide' }, { type: 'separator' }, { role: 'quit' }]
+		});
+
+		// Window menu.
+		menuTemplate[2].submenu = [{ role: 'close' }, { role: 'minimize' }, { role: 'zoom' }, { type: 'separator' }, { role: 'front' }];
+	}
+	var appMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(appMenu);
+}
+
 app.on('ready', function () {
+	createMenu();
+
 	createWindow();
 
 	// Debugging.
-	mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 
 	// mainWindow.webContents.on( 'dom-ready', function() {
 	// });
